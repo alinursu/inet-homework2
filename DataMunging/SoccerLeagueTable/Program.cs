9cd1cb516910e596859753500640fe1b0cd8a866
Stefan-Alin Ursu <alinstefan.ursu@endava.com>
2021-10-07 14:01:50 +0300

    Finished soccer league table logic.

--- DataMunging/SoccerLeagueTable/Program.cs
+++ DataMunging/SoccerLeagueTable/Program.cs
@@ -24,6 +24,28 @@ namespace SoccerLeagueTable
 				}
             }
 
+			// Choosing the team with the smallest difference between 'for' and 'against' number of goals
+			int chosenTeamDifference = 9999;
+			Team chosenTeam = null;
+			foreach (Team team in teams)
+			{
+				if (Math.Abs(team.GoalsFor - team.GoalsAgainst) < chosenTeamDifference)
+				{
+					chosenTeam = team;
+					chosenTeamDifference = Math.Abs(team.GoalsFor - team.GoalsAgainst);
+				}
+			}
+
+			if(chosenTeam != null)
+            {
+                Console.WriteLine("Team " + chosenTeam.Name + " is the team with the smallest difference between 'for' " +
+					"and 'against' number of goals (" + chosenTeamDifference + ").");
+            }
+			else
+            {
+                Console.WriteLine("No teams found in football.dat");
+            }
+
 			Console.WriteLine("Press any key to exit.");
 			Console.ReadKey();
 		}
